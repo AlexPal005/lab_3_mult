805d7919bfd33e9bb5080d20001789ac4bfcb8bc
AlexPal005 <rudenkoolexandr06@knu.ua>
2022-06-19 22:53:25 +0300

    add threads

---
+++ lab_3a_mult/Tree.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <thread>
+#include <time.h> 
+using namespace std;
+/**
+*  Структура вузла, що містить ключ, вказівники на батька та потомків, колір вузла
+*/
+struct node {
+	int key;
+	node* left;
+	node* right;
+	node* parent;
+	char color;
+};
+class Tree {
+public:
+	Tree() {
+		q = NULL;
+		root = NULL;
+	}
+	/**Вставка елемента*/
+	void insert(int z);
+	void insertfix(node*);
+	/**Лівий поворот*/
+	void leftrotate(node*);
+	/**Правий поворот */
+	void rightrotate(node*);
+	/**Показати дерево*/
+	void disp();
+	void display(node*);
+	/**Видалити елемент*/
+	void del(int val);
+	void delfix(node* p);
+	node* successor(node*);
+	/**Пошук елемента*/
+	node* search(int value);
+private:
+	/** Вказівник на корінь*/
+	node* root;
+	node* q;
+
+
+};
--- lab_3a_mult/lab_3a_mult.cpp
+++ lab_3a_mult/lab_3a_mult.cpp
@@ -1,20 +1,47 @@
-﻿// lab_3a_mult.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include "Tree.h"
+int main() {
+    int ch, y = 0;
+    Tree obj;
+    do
+    {
+        cout << "\n\t RED BLACK TREE ";
+        cout << "\n 1. Insert in the tree ";
+        cout << "\n 2. Delete a node from the tree";
+        cout << "\n 3. Search for an element in the tree";
+        cout << "\n 4. Display the tree ";
+        cout << "\n 5. Exit ";
+        cout << "\nEnter Your Choice: ";
+        cin >> ch;
+        switch (ch)
+        {
+        case 1:
+            cout << "\nEnter key of the node to be inserted: ";
+            int z;
+            cin >> z;
+            obj.insert(z);
+            cout << "\nNode Inserted.\n";
+            break;
+        case 2:
+            int v;
+            cout << "\nEnter the key of the node to be deleted: ";
+            cin >> v;
+            obj.del(v);
+            break;
+        case 3:
+            int x;
+            cout << "\n Enter key of the node to be searched: ";
+            cin >> x;
+            obj.search(x);
+            break;
+        case 4: 
+            obj.disp();
+            break;
+        case 5: y = 1;
+            break;
+        default: cout << "\nEnter a Valid Choice.";
+        }
+        cout << endl;
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+    } while (y != 1);
+    return 1;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
